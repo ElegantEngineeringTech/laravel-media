1156086e740704a137882ca6741c9241c478b5ba
Quentin Gabriele <quentin.gabriele@gmail.com>
2024-01-10 16:27:32 +0100

    add video component

---
+++ resources/views/components/video.blade.php
@@ -0,0 +1,11 @@
+@props([
+    'conversion' => null,
+    'poster' => null,
+    'src' => null,
+    'media',
+])
+
+<video {!! $attributes !!} height="{{ $media->getHeight($conversion) }}" width="{{ $media->getWidth($conversion) }}"
+    alt="{{ $media->getName($conversion) }}"
+    poster="{{ $poster ?? $media->getUrl($conversion ? $conversion . '.poster' : 'poster') }}"
+    src="{{ $src ?? $media->getUrl() }}">
