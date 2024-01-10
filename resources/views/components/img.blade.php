0297854659bfa7177f3c693817e4dfe14ab83a3c
Quentin Gabriele <quentin.gabriele@gmail.com>
2024-01-10 16:13:11 +0100

    use generic js

--- resources/views/components/img.blade.php
+++ resources/views/components/img.blade.php
@@ -9,15 +9,5 @@
     height="{{ $media->getHeight($conversion) }}" width="{{ $media->getWidth($conversion) }}"
     alt="{{ $media->getName($conversion) }}"
     @if ($responsive) srcset="{{ $media->getSrcset($conversion)->join(', ') }}" 
-    x-data="{
-        sizes: '1px',
-        resize() {
-            this.$nextTick(() => {
-                setTimeout(() => {
-                    size = this.$el.getBoundingClientRect().width ?? 1;
-                    this.sizes = Math.ceil(size / window.innerWidth * 100) + 'vw';
-                }, 50);
-            });
-        }
-    }"
-    x-intersect.once="resize" x-bind:sizes="sizes" @endif>
+    onload="window.requestAnimationFrame(function(){if(!(size=getBoundingClientRect().width))return;onload=null;sizes=Math.ceil(size/window.innerWidth*100)+'vw';});"
+    sizes="1px" @endif>
