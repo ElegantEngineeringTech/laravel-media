e786b7bcbe35014329409e214b29982cc33edf30
Quentin Gabriele <quentin.gabriele@gmail.com>
2024-07-12 18:38:48 +0200

    fix video

--- resources/views/components/video.blade.php
+++ resources/views/components/video.blade.php
@@ -2,14 +2,18 @@
     'conversion' => null,
     'poster' => null,
     'src' => null,
+    'autoplay' => false,
+    'muted' => muted,
+    'playsinline' => playsinline,
+    'loop' => loop,
     'media',
 ])
 
-<video {!! $attributes->merge([
-    'style' => $media->aspect_ratio ? "aspect-ratio:{$media->aspect_ratio};" : null,
-]) !!} height="{{ $media->getHeight($conversion) }}" width="{{ $media->getWidth($conversion) }}"
+<video {!! $attributes !!} height="{{ $media->getHeight($conversion) }}" width="{{ $media->getWidth($conversion) }}"
     alt="{{ $media->getName($conversion) }}"
     poster="{{ $poster ?? $media->getUrl($conversion ? $conversion . '.poster' : 'poster') }}"
-    src="{{ $src ?? $media->getUrl() }}">
+    src="{{ $src ?? $media->getUrl($conversion) }}" @if ($autoplay) autoplay @endif
+    @if ($muted) muted @endif @if ($playsinline) playsinline @endif
+    @if ($loop) loop @endif>
     {{ $slot }}
 </video>
