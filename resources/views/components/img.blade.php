8aadd3ecc4e14f94275ae855daa23715c0dc88e0
Quentin Gabriele <quentin.gabriele@gmail.com>
2025-03-13 13:45:40 +0100

    unescape src in blade

--- resources/views/components/img.blade.php
+++ resources/views/components/img.blade.php
@@ -10,8 +10,7 @@
     'height' => null,
 ])
 
-<img {!! $attributes !!} loading="{{ $loading }}"
-    src="{{ $src ?? $media->getUrl($conversion, $fallback, $parameters) }}"
+<img {!! $attributes !!} loading="{{ $loading }}" src="{!! $src ?? $media->getUrl($conversion, $fallback, $parameters) !!}"
     height="{{ $height ?? $media->getHeight($conversion, $fallback) }}"
     width="{{ $width ?? $media->getWidth($conversion, $fallback) }}"
     alt="{{ $alt ?? $media->getName($conversion, $fallback) }}">
--- resources/views/components/video.blade.php
+++ resources/views/components/video.blade.php
@@ -15,10 +15,9 @@
     'loop' => false,
 ])
 
-<video {!! $attributes !!} src="{{ $src ?? $media->getUrl($conversion, $parameters) }}"
-    height="{{ $height ?? $media->getHeight($conversion) }}" width="{{ $width ?? $media->getWidth($conversion) }}"
-    alt="{{ $alt ?? $media->getName($conversion) }}" poster="{{ $poster ?? $media->getUrl($posterConversion) }}"
-    {{ when($autoplay, 'autoplay') }} {{ when($muted, 'muted') }} {{ when($playsinline, 'playsinline') }}
-    {{ when($loop, 'loop') }}>
+<video {!! $attributes !!} src="{!! $src ?? $media->getUrl($conversion, $parameters) !!}" height="{{ $height ?? $media->getHeight($conversion) }}"
+    width="{{ $width ?? $media->getWidth($conversion) }}" alt="{{ $alt ?? $media->getName($conversion) }}"
+    poster="{{ $poster ?? $media->getUrl($posterConversion) }}" {{ when($autoplay, 'autoplay') }}
+    {{ when($muted, 'muted') }} {{ when($playsinline, 'playsinline') }} {{ when($loop, 'loop') }}>
     {{ $slot }}
 </video>
