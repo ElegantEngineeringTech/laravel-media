7fa7a0525507f8a310840596610992ed1f2af116
Quentin Gabriele <quentin.gabriele@gmail.com>
2025-06-24 18:05:46 +0200

    add wav audio extraction

--- src/FFMpeg/Video.php
+++ src/FFMpeg/Video.php
@@ -66,6 +66,16 @@ class Video extends FFMpeg
         return $this->ffmpeg("-i {$input} -vn -acodec libmp3lame -b:a 128k {$output}");
     }
 
+    /**
+     * @return array{0: int, 1: string[]}
+     */
+    public function wav(
+        string $input,
+        string $output,
+    ): array {
+        return $this->ffmpeg("-i {$input} -vn -acodec pcm_s16le -ar 44100 -ac 2 {$output}");
+    }
+
     protected function getScale(?int $width = null, ?int $height = null): ?string
     {
         if ($width && $height) {
