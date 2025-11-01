12245afe07cf5d843c16afcfaaeac6bdc1e7126b
Quentin Gabriele <quentin.gabriele@gmail.com>
2025-11-01 19:21:26 +0100

    better video blade

--- resources/views/components/video.blade.php
+++ resources/views/components/video.blade.php
@@ -18,16 +18,27 @@
 ])
 
 @php
-    $url =
-        $src ??
-        $media->getUrl(conversion: $conversion, fallback: $fallback, parameters: $parameters, dispatch: $dispatch);
 
-    $posterUrl = $poster ?? $media->getUrl(conversion: $posterConversion, dispatch: $posterDispatch);
+    $source = $conversion
+        ? $media->getConversion(
+            name: $conversion,
+            state: \Elegantly\Media\Enums\MediaConversionState::Succeeded,
+            fallback: is_bool($fallback) ? null : $fallback,
+            dispatch: $dispatch,
+        )
+        : null;
+
+    $source ??= $fallback === true ? $media : null;
+
+    if ($posterConversion) {
+        $poster ??= $media->getUrl(conversion: $posterConversion, dispatch: $posterDispatch);
+    }
+
 @endphp
 
-<video {!! $attributes !!} src="{!! $url !!}" height="{{ $height ?? $media->getHeight($conversion) }}"
-    width="{{ $width ?? $media->getWidth($conversion) }}" alt="{{ $alt ?? $media->getName($conversion) }}"
-    poster="{{ $posterUrl }}" {{ when($autoplay, 'autoplay') }} {{ when($muted, 'muted') }}
-    {{ when($playsinline, 'playsinline') }} {{ when($loop, 'loop') }}>
+<video {!! $attributes !!} src="{!! $src ?? $source->url !!}" height="{{ $height ?? $source->height }}"
+    width="{{ $width ?? $source->width }}" alt="{{ $alt ?? $source->name }}" poster="{{ $poster }}"
+    {{ when($autoplay, 'autoplay') }} {{ when($muted, 'muted') }} {{ when($playsinline, 'playsinline') }}
+    {{ when($loop, 'loop') }}>
     {{ $slot }}
 </video>
